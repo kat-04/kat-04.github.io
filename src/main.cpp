a35c591bf1e39c3dfa05664ced4a0fdc0635449e
Katherine Lu <katherinelu@katherinesmbp9.wifi.local.cmu.edu>
2023-04-17 12:16:18 -0400

    CMake updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
-#include "gol-sequential.cpp"
-#include "gol-cuda.cpp"
+#include "gol-sequential.h"
+/* #include "gol-cuda.cpp" */
 
 int main(int argc, char** argv)
 {
     // TODO: call golCuda with args (maybe add option flag to input to decide which version to call)
-    return gol_cuda(argc, argv);
-    //return golSequential(argc, argv);
+    /* return gol_cuda(argc, argv); */
+
+    // clean output files
+    system("rm output-files/*.txt");
+
+    return golSequential(argc, argv);
 }
