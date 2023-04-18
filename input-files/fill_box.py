ac2a816898ea13bb85f323d71fc6cfbb35430dfd
Katherine Lu <katherine.lu06@gmail.com>
2023-04-17 20:42:29 -0400

    Added test cases

---
+++ input-files/fill_box.py
@@ -0,0 +1,26 @@
+import sys
+import argparse
+import random
+
+parser = argparse.ArgumentParser(
+                    prog='fill_box.py',
+                    description='Creates input file of vertices with entire cube of side length n filled')
+
+parser.add_argument('filename')
+parser.add_argument('-s', '--size', type=int)
+parser.add_argument('-r', '--rule')
+
+args = parser.parse_args()
+
+n = args.size
+
+file = args.filename
+with open(file, 'w') as f:
+    f.write(f"{args.rule}\n")
+    for x in range(n):
+        for y in range(n):
+            for z in range(n):
+                if (random.random() < 0.6):
+                    f.write(f"{x} {y} {z}\n");
+                # if ((x + y + z) % 10 != 0):
+                #     f.write(f"{x} {y} {z}\n");
