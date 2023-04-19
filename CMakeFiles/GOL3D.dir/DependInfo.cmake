9e302b31e1cf333ace8cc781aa037158af27caf3
Katherine Lu <katherine.lu06@gmail.com>
2023-04-18 20:57:30 -0400

    Added milestone report to github page

---
+++ CMakeFiles/GOL3D.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/katherinelu/Documents/15418/kat-04.github.io/src/gol-sequential.cpp" "CMakeFiles/GOL3D.dir/src/gol-sequential.cpp.o" "gcc" "CMakeFiles/GOL3D.dir/src/gol-sequential.cpp.o.d"
+  "/Users/katherinelu/Documents/15418/kat-04.github.io/src/main-local.cpp" "CMakeFiles/GOL3D.dir/src/main-local.cpp.o" "gcc" "CMakeFiles/GOL3D.dir/src/main-local.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
