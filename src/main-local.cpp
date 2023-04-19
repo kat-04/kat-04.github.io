2dfa04819254452cc18d085415ca1dba2d9757d2
katepete8 <kpeterse@andrew.cmu.edu>
2023-04-18 21:18:17 -0400

    main ssh and local sync

--- src/main-local.cpp
+++ src/main-local.cpp
@@ -1,11 +1,10 @@
+#include <iostream>
+#include <string>
 #include "gol-sequential.h"
-/* #include "gol-cuda.cpp" */
+#include "gol-cuda.cpp"
 
 int main(int argc, char** argv)
 {
-    // TODO: call golCuda with args (maybe add option flag to input to decide which version to call)
-    /* return gol_cuda(argc, argv, "output-files"); */
-
     // clean output files
     system("rm -rf output-files/");
     system("mkdir output-files/");
