903fe83de98deafbccbb0d6a04df8d67c7cdac67
katepete8 <kpeterse@andrew.cmu.edu>
2023-05-04 14:29:32 -0400

    extra timing code

--- src/main-ssh.cpp
+++ src/main-ssh.cpp
@@ -3,6 +3,7 @@
 #include "gol-sequential.h"
 #include "gol-cuda.cpp"
 #include "gol-openmp.h"
+#include "timing.h"
 
 // runs game of life for parsed arguments on ghc machines
 int main(int argc, char** argv)
@@ -22,17 +23,25 @@ int main(int argc, char** argv)
     if (argc == 4) {
         return golSequential(argc, argv, outputDir);
     } 
+    int result = 0; 
     if (argc == 5 || argc == 6) { // version specified 
         string version = argv[4];
         if (version == "seq") {
-            return golSequential(argc, argv, outputDir);
+            Timer totalTimer;
+            result = golSequential(argc, argv, outputDir);
+            std::cout << "Total sequential time: " << totalTimer.elapsed() << "s" << std::endl;
         }
         if (version == "cuda") {
-            return gol_cuda(argc, argv, outputDir);
+            Timer totalTimer;
+            result = gol_cuda(argc, argv, outputDir);
+            std::cout << "Total CUDA time: " << totalTimer.elapsed() << "s" << std::endl;
         }
         if (version == "omp") {
-            return golOpenMP(argc, argv, outputDir);
+            Timer totalTimer;
+            result = golOpenMP(argc, argv, outputDir);
+            std::cout << "Total OpenMP time: " << totalTimer.elapsed() << "s" << std::endl;
         }
+        return result;
     }
     cerr << "Usage: " << argv[0] << " input_file number_of_frames side_length version:[seq(default)/cuda/omp]" << endl;
     return 1;
